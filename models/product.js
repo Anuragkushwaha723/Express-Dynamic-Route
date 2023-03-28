da1ea35bb1cf59c126fd3c0952becc260ea972fd
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-03-28 21:52:12 +0530

    Adding edit and delete functionality

--- models/product.js
+++ models/product.js
@@ -18,7 +18,8 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
+  constructor(id,title, imageUrl, description, price) {
+    this.id=id;
     this.title = title;
     this.imageUrl = imageUrl;
     this.description = description;
@@ -26,12 +27,21 @@ module.exports = class Product {
   }
 
   save() {
-    this.id=Math.random().toString();
     getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
+      if(this.id){
+        const existingProductIndex=products.findIndex(prod=>prod.id===this.id);
+        const updatedProducts=[...products];
+        updatedProducts[existingProductIndex]=this;
+        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+          console.log(err);
+        });
+      }else{
+        this.id=Math.random().toString();
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+      }
     });
   }
 
@@ -43,5 +53,27 @@ module.exports = class Product {
       const product=products.find(p=> p.id===id);
       cb(product);
     })
+  }
+
+  static deleteproductbyID(id,cb){
+    getProductsFromFile((products)=>{
+      const deleteElementIndex=products.findIndex(prod=>prod.id===id);
+      let updatedProducts=[];
+      for(var i=0;i<products.length;i++){
+        if(products[i].id===id){
+          continue;
+        }else{
+          updatedProducts.push(products[i]);
+        }
+      }
+      fs.writeFile(p,JSON.stringify(updatedProducts),(err)=>{
+        if(err){
+          console.log(err);
+        }else{
+          cb();
+        }
+        
+      })
+    });
   }
 };
